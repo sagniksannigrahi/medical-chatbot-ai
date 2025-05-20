749bcac0f4234011989c87a97a6bb96769ae6abf
SAGNIK SANNIGRAHI <166724040+sagniksannigrahi@users.noreply.github.com>
2025-05-20 22:31:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ def chat():
     return str(answer)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port= port, debug= True)
